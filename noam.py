a8d7c7a57e0b083351365629648ae3e6710d5766
Jack <jack.grahl@yahoo.co.uk>
2016-03-04 22:39:03 +0000

    Add main file.

---
+++ noam.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import pygame
+from window import *
+
+if __name__ == "__main__":
+    # Initialise the game engine
+    pygame.init()
+    # Create the window/game/b
+    window = Window()
+
+    while True:
+        window.main()
+        pygame.display.update()
