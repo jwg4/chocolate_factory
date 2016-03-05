f8ec26e8c63cc4ca3281e2eb13f969949a427a3c
Jack <jack.grahl@yahoo.co.uk>
2016-03-05 08:09:05 +0000

    Move Noam-specific stuff out into character.py._

---
+++ character.py
@@ -0,0 +1,30 @@
+import os
+
+import pygame
+
+import dimensions
+
+ASSETS_DIR = 'images'
+
+class Character(object):
+    def __init__(self, window):
+        self.window = window
+        self.image = pygame.image.load(os.path.join(ASSETS_DIR, self.image_location))
+
+    def draw(self):
+        self.window.blit(self.image, self.locations[self.position])
+
+    def down(self):
+        if self.position > 0:
+            self.position = self.position - 1
+
+    def up(self):
+        if self.position < 2:
+            self.position = self.position + 1
+
+class Noam(Character):
+    locations = dimensions.CHARACTER_A
+    position = 0
+    image_location = "characters/noam.png"
+
+
--- window.py
+++ window.py
@@ -4,22 +4,21 @@ import pygame
 from pygame.locals import *
 
 import dimensions
+from character import Noam
 
 ASSETS_DIR = 'images'
 BLACK = (  0,   0,   0)
 FPS = 59.37
 
 class Window(object):
-    locations = dimensions.CHARACTER_A
-    x = 0
 
     def __init__(self):
         self.window = pygame.display.set_mode(dimensions.WINDOW_SIZE)
         self.clock = pygame.time.Clock()
         self.background = pygame.image.load(os.path.join(ASSETS_DIR, "background.png"))
+        self.character = Noam(self.window)
 
         self.init()
-        self.x = 0
 
     def init(self):
         # Draw
@@ -37,7 +36,7 @@ class Window(object):
     def draw(self):
         self.window.blit(self.background, (0,0))
         self.draw_conveyor()
-        self.draw_character()
+        self.character.draw()
 
     def draw_conveyor(self):
         image = pygame.image.load(os.path.join(ASSETS_DIR, "conveyor.png"))
@@ -47,18 +46,12 @@ class Window(object):
                 y = 105 + i * 70
                 self.window.blit(image, (x, y))
 
-    def draw_character(self):
-        image = pygame.image.load(os.path.join(ASSETS_DIR, "characters/noam.png"))
-        self.window.blit(image, self.locations[self.x])
-
     def listen_for_input(self):
         # Discrete key presses
         for event in pygame.event.get():
             if (event.type == pygame.KEYDOWN):
                 if (event.key == pygame.K_DOWN):
-                    if self.x > 0:
-                        self.x = self.x - 1
+                    self.character.down()
                 elif (event.key == pygame.K_UP):
-                    if self.x < 2:
-                        self.x = self.x + 1
+                    self.character.up()
                 break
