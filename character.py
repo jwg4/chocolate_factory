3ba5bb76a3a51102f68c9aeecaf6532776526b90
Jack <jack.grahl@yahoo.co.uk>
2016-10-07 23:40:30 +0100

    Fix all the spacing issues with this file.

--- character.py
+++ character.py
@@ -6,10 +6,12 @@ import dimensions
 
 ASSETS_DIR = 'images'
 
+
 class Character(object):
     def __init__(self, window):
         self.window = window
-        self.image = pygame.image.load(os.path.join(ASSETS_DIR, self.image_location))
+        image_path = os.path.join(ASSETS_DIR, self.image_location)
+        self.image = pygame.image.load(image_path)
 
     def draw(self):
         self.window.blit(self.image, self.locations[self.position])
@@ -22,15 +24,14 @@ class Character(object):
         if self.position < 2:
             self.position = self.position + 1
 
+
 class Noam(Character):
     locations = dimensions.CHARACTER_A
     position = 0
     image_location = "characters/noam.png"
 
+
 class Rosa(Character):
     locations = dimensions.CHARACTER_B
     position = 0
     image_location = "characters/rosa.png"
-
-
-
